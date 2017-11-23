c14c667ce3cdf5ede016cd5702114cddb086bccb
Alex Jacobs <a1603181@myy.haaga-helia.fi>
2017-11-23 14:06:37 +0200

    fixes

--- src/main/java/fi/hh/server/hockey/HockeyApplication.java
+++ src/main/java/fi/hh/server/hockey/HockeyApplication.java
@@ -2,15 +2,23 @@ package fi.hh.server.hockey;
 
 import fi.hh.server.hockey.domain.User;
 import fi.hh.server.hockey.domain.UserRepository;
+import javafx.application.Application;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class HockeyApplication {
+public class HockeyApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(HockeyApplication.class, args);
 	}
 
