4a9969a67528653d794d478102464d740b4138b2
Alex Jacobs <a1603181@myy.haaga-helia.fi>
2017-11-23 14:44:23 +0200

    unbroke everything

--- src/main/java/fi/hh/server/hockey/HockeyApplication.java
+++ src/main/java/fi/hh/server/hockey/HockeyApplication.java
@@ -5,19 +5,12 @@ import fi.hh.server.hockey.domain.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class HockeyApplication extends SpringBootServletInitializer {
+public class HockeyApplication {
 
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(HockeyApplication.class);
-    }
-
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(HockeyApplication.class, args);
 	}
 
@@ -26,7 +19,7 @@ public class HockeyApplication extends SpringBootServletInitializer {
         return (args) -> {
 
             // Create new users
-            User user1 = new User("user","$2a$04$zdGl8n9CPo7bKcURxZyBIeedqGpuR7b/B7051RZfotU5bFy00606i1", "USER");
+            User user1 = new User("user","$2a$04$hNvP6URpBfjyaq4hleQuOuvWQ2HLZErkz8Si7oyWIqaqmBTA4Ft.K", "USER");
             User user2 = new User("admin","$2a$04$W1UHL2Hxfy88U/koDH3BquRgXC8aae4RUiM03JDguIfzsfnh6bFVi", "ADMIN");
             urepository.save(user1);
             urepository.save(user2);
